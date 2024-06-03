7eedf801f303d0127cb2011af4bca93e8d2bcad5
Yeasir Arefin Tusher <yeasir.arefin@bjitgroup.com>
2024-06-03 09:43:42 +0600

    Initial Commit

---
+++ Analog Clock/Analog_ClockApp.swift
@@ -0,0 +1,21 @@
+//
+//  Analog_ClockApp.swift
+//  Analog Clock
+//
+//  Created by YeasirArefinTusher-11702 on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Analog_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
